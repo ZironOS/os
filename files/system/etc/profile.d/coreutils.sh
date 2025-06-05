864a8a54d79d3b92324cd751a5ea2cb094cf8452
SapryWenInera <97859194+SapryWenInera@users.noreply.github.com>
2025-06-04 22:51:16 -0300

    Rework cli packages with further modularization

---
+++ files/system/etc/profile.d/coreutils.sh
@@ -0,0 +1,40 @@
+# Core Utils QoL Improvements
+
+# Check if eza is present if not fallback to ls
+function ls() {
+	local LS="ls"
+
+	if command -v eza &>/dev/null; then
+		LS=eza
+	fi
+	$LS "$@"
+}
+
+# Check if ripgrep is present if not regular grep is used as fallback
+function grep() {
+	local GREP="grep"
+	if command -v rg &>/dev/null; then
+		GREP=rg
+	fi
+	if [ "$1" == "-E" ] && [ "$GREP" == "rg" ]; then
+		shift 1
+		/usr/bin/rg -e "$@"
+	elif [ "$1" == "-E"] && [ "$GREP" != "rg" ]; then
+		/usr/bin/grep "$@"
+	elif [ $GREP == "rg" ]; then
+		/usr/bin/rg "$@"
+	else
+		/usr/bin/grep "$@"
+	fi
+}
+
+# Check if bat is present and if not uses cat as fallback
+function cat() {
+	local CAT="cat"
+
+	if command -v bat &>/dev/null; then
+		CAT=bat
+	fi
+
+	$CAT "$@"
+}
